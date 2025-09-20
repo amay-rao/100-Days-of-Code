f75a9b84e4cd67929c511226b0b1aa90536567f7
amay-rao <krrishrao5@gmail.com>
2025-09-20 10:00:00 +0530

    Create Day31 Q1.c

---
+++ Day31 Q1.c
@@ -0,0 +1,27 @@
+//Q61: Search for an element in an array using linear search.
+#include <stdio.h>
+int main() {
+    int n, i, key;
+    int arr[100];
+    int found = -1;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            found = i;
+            break;
+        }
+    }
+    if (found != -1) {
+        printf("Found at index %d", found);
+    } else {
+        printf("-1");
+    }
+    return 0;
+}
